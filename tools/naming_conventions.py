8504829921711844bfcb6d361e61496d64fa371d
albion2000 <albion2000@ufowaves.org>
2018-01-12 23:23:40 +0100

    naming conventions : better dir count. Clear error message is renaming is refused by the OS.

--- tools/naming_conventions.py
+++ tools/naming_conventions.py
@@ -39,19 +39,22 @@ def accentsTidy(s) :
 logFile = open('logRename.txt','a')
 
 def printandlog(str):
-  print(str)
-  logFile.write(str+"\n")
-  return
+	print(str)
+	logFile.write(str+"\n")
+	return
 
 def printandlogNoRC(str):
-  print(str, end='')
-  logFile.write(str)
-  return
+	print(str, end='')
+	logFile.write(str)
+	return
 
 def printandlogTuple(tuple):
-  print(tuple)
-  logFile.write(''.join(tuple))
-  return
+	print(tuple)
+	logFile.write(''.join(tuple))
+	return
+
+def same_string(a,b):
+	return ((a.find(b)==0) and (b.find(a)==0))
 
 def process(do_rename) :
 
@@ -63,40 +66,63 @@ def process(do_rename) :
 	# Set the directory you want to start from
 	rootDir = '.'
 	nbChanges = 0
-	found = 1
-	while (found) :
-		found = 0
+
+# Simulation : we only parse the subs, don't need the full path
+	if (not do_rename):
+		nbDir = 0
 		for dirName, subdirList, fileList in os.walk(rootDir):
-			stripped = accentsTidy(dirName)
-			if ((stripped.find(dirName)==0) and (dirName.find(stripped)==0)) : 
-				printandlogNoRC('.'); 
-				sys.stdout.flush()
-			else :
-				if (not found) : 
+			for subdir in subdirList :
+				stripped = accentsTidy(subdir)
+				nbDir = nbDir + 1
+				if (same_string(stripped,subdir)) : 
+					printandlogNoRC('.'); 
+					sys.stdout.flush()
+				else :
 					nbChanges = nbChanges + 1 
-					if (do_rename) :
+					printandlog("\n"+dirName+"\\"+stripped+"  <--  "+dirName+"\\"+subdir); 
+	else:
+# Real Deal, we parse all the dirs using path from the rootDir, one by one. Slow, not smart, but small code.
+		found = 1
+		while (found) :
+			found = 0
+			nbDir = 0
+			for dirName, subdirList, fileList in os.walk(rootDir):
+				if dirName == '.' :
+					continue
+				if dirName == '..' :
+					continue
+				stripped = accentsTidy(dirName)
+				nbDir = nbDir + 1
+				if (same_string(stripped,dirName)) : 
+					printandlogNoRC('.'); 
+					sys.stdout.flush()
+				else :
+					if (not found) : 
+						nbChanges = nbChanges + 1 
 						printandlog("");
-						printandlogNoRC(stripped+"  <--  "+dirName); 
-						os.rename(dirName,stripped)
+						printandlog(stripped+"  <--  "+dirName); 
+						try:
+							os.rename(dirName,stripped)
+						except:
+							print("Renaming was refused by the PC. Check that you don't have a file open in the file tree. You might need to close some file explorer");
+							sys.exit()
 						found = 1
 						break
-					else :
-						printandlog(stripped+"  <--  "+dirName); 
 					
 
 
 	# BELL	
 	print('\a')
-	printandlog('\r\n')
+	printandlog('\n')
 	
 	if (nbChanges==0):
-		printandlog("no changes needed, all directories already in compliance")
+		printandlog("no change needed, all %i directories are already in compliance" % nbDir)
 	else:
 		if (do_rename):
-			printandlog("%i changes done" % nbChanges)
+			printandlog("%i change(s) done in a total of %i directories" % (nbChanges,nbDir))
 		else:
-			printandlog("%i changes needed" % nbChanges)
-			printandlog("this was a simulation, if you are happy with this renaming proposal, do 'naming_coventions -w'");
+			printandlog("%i change(s) needed in a total of %i directories" % (nbChanges,nbDir))
+			printandlog("this was a simulation, if you are happy with this renaming proposal, do 'naming_conventions -w'");
 
 def test() :
 	s = 'æÁÀÂÄÃÅÇÉÈÊËÍÏÎÌÑÓÒÔÖÕÚÙÛÜÝœ- áàâäãåçéèêëíìîïñóòôöõúùûüý ÿ'
