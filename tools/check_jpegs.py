528e56e63bf1bbf28b2bb4a30c1d1af77aabaaaf
albion2000 <albion2000@ufowaves.org>
2025-07-21 20:36:12 +0200

    espaces plutot que tabs. relaxation on rename, acces upper caps.

--- tools/check_jpegs.py
+++ tools/check_jpegs.py
@@ -3,103 +3,119 @@ import os
 from PIL import Image
 import sys
 
+logFileName = 'check_jpegs.log'
+logFile = open(logFileName,'a', encoding="utf-8")
+
+def printandlog(str):
+    print(str)
+    logFile.write(str+"\n")
+    return
+
+def printandlogNoRC(str):
+    print(str, end='')
+    logFile.write(str)
+    return
+
 
 # this code also should work for other compressions, with little changes
 
 def isJpeg(fname):
-	if (fname.lower().find(".jpg") == len(fname)-4) :
-		return True
-	if (fname.lower().find(".jpeg") == len(fname)-4) :
-		return True
-	return False
+    if (fname.lower().endswith(".jpg")) :
+        return True
+    if (fname.lower().endswith(".jpeg")) :
+        return True
+    return False
 
 def process(fullCheck):
 
-	# Set the directory you want to start from
-	rootDir = '.'
-	currentPath = os.getcwd()
+    # Set the directory you want to start from
+    rootDir = '.'
+    currentPath = os.getcwd()
 
-	print('Searching for corrupted .jpg and .jpeg files')
-	print('Each . shows that one more directory was parsed')
-	if (fullCheck):
-		print('Full sanity check')
-	else:
-		print('This step does not check the whole file, but only the header and some consistency')
-	print('Processing directory '+currentPath)
-	print('Press <ctrl>+c to abort')
-	badFilesCount = 0
-	allJPGFilesCount = 0
-	allDirCount = 0
-	for dirName, subdirList, fileList in os.walk(rootDir):
-		print('.', end=''); 
-		sys.stdout.flush()
-		files = ();
-		found = 0;
-		currentPath = os.getcwd();
-		for fname in fileList:
-	#		print('\t%s' % fname)
-			if (isJpeg(fname)):
-				found = 1;
-				allJPGFilesCount = allJPGFilesCount+1
-				newTuple = (dirName+'/'+fname,);
-				files = files + newTuple
-				try :
-					im = Image.open(dirName+'/'+fname)
-	# that's one way to enforce reading the whole file, but since it is not faster than img2pdf, this is not interesting.
-	# but could be for a general full sanity check
-					if (fullCheck):
-						box = (10, 10, 20, 20)
-						region = im.crop(box)
-				except IOError :
-					print('\r\n'+dirName+'/'+fname+"----->invalid file, not a jpeg ?",end='')
-					badFilesCount = badFilesCount+1
-		if (found==1) :
-			allDirCount = allDirCount + 1
+    printandlog('Searching for corrupted .jpg and .jpeg files')
+    printandlog('Each . shows that one more directory was parsed')
+    if (fullCheck):
+        printandlog('Full sanity check')
+    else:
+        printandlog('This step does not check the whole file, but only the header and some consistency')
+        printandlog('Use check_jpeg_full for a deeper sanity check')
+    printandlog('Processing directory '+currentPath)
+    print('Press <ctrl>+c to abort')
+    badFilesCount = 0
+    allJPGFilesCount = 0
+    allDirCount = 0
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        print('.', end=''); 
+        sys.stdout.flush()
+        files = ();
+        found = 0;
+        currentPath = os.getcwd();
+        for fname in fileList:
+    #       print('\t%s' % fname)
+            if (isJpeg(fname)):
+                found = 1;
+                allJPGFilesCount = allJPGFilesCount+1
+                newTuple = (dirName+'/'+fname,);
+                files = files + newTuple
+                try :
+                    im = Image.open(dirName+'/'+fname)
+    # that's one way to enforce reading the whole file, but since it is not faster than img2pdf, this is not interesting.
+    # but could be for a general full sanity check
+                    if (fullCheck):
+                        box = (10, 10, 20, 20)
+                        region = im.crop(box)
+                except IOError :
+                    printandlogNoRC('\r\n'+dirName+'/'+fname+"----->invalid file, not a jpeg ?")
+                    badFilesCount = badFilesCount+1
+        if (found==1) :
+            allDirCount = allDirCount + 1
 
-	print('\r\n')
-	if (badFilesCount>0) :
-		print('found %i bad file(s) among %i jpeg files in %i directories' % (badFilesCount,allJPGFilesCount,allDirCount));
-	else :
-		print('All %i jpeg files in %i directories (containing jpeg images) passed the sanity check. No problem.' % (allJPGFilesCount,allDirCount));
+    printandlog('\r\n')
+    if (badFilesCount>0) :
+        logs = f"found {badFilesCount} bad file(s) among {allJPGFilesCount} jpeg files in {allDirCount} directories"
+        printandlog(logs);
+    else :
+        logs = f"All {allJPGFilesCount} jpeg files in {allDirCount} directories (containing jpeg images) passed the sanity check. No problem."
+        printandlog(logs);
 
-	# BELL	
-	print('\a')
-#	os.system("pause")
+    # BELL  
+    print('\a')
+#   os.system("pause")
 
 
 def print_syntax() :
-	print('syntax for just checking the file headers, for quick validation purposes:')
-	print('check_jpegs.py')
-	print('syntax for full file checking:')
-	print('check_jpegs.py -f')
-	os.system("pause")
+    print('syntax for just checking the file headers, for quick validation purposes:')
+    print('check_jpegs.py')
+    print('syntax for full file checking:')
+    print('check_jpegs.py -f')
+    os.system("pause")
 
 
 
 def main(argv) :
-	if (len(sys.argv)==1): 
-		process(0)
-		os.system("pause")
-	else : 
-		try:
-			opts, args = getopt.getopt(argv,"hf")
-		except getopt.GetoptError:
-			print_syntax()
-			sys.exit(2)
-		for opt, arg in opts:
-			if opt == '-h':
-				print_syntax()
-				sys.exit()
-			elif opt == '-f':
-				process(1)
-				os.system("pause")
-				sys.exit()
-			else : 
-				print("ignored");
-				os.system("pause")
-				sys.exit()
-		print_syntax()
+    if (len(sys.argv)==1): 
+        process(0)
+        os.system("pause")
+    else : 
+        try:
+            opts, args = getopt.getopt(argv,"hf")
+        except getopt.GetoptError:
+            print_syntax()
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt == '-h':
+                print_syntax()
+                sys.exit()
+            elif opt == '-f':
+                process(1)
+                os.system("pause")
+                sys.exit()
+            else : 
+                print("ignored");
+                os.system("pause")
+                sys.exit()
+        print_syntax()
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
 
--- tools/check_path.py
+++ tools/check_path.py
@@ -1,28 +1,31 @@
 import os
 import re
 
-# identify too long path and display 10 longest.
+# identify too long path and display 50 longest.
 
-nb_long_path = 10
+nb_long_path = 50
 long_path = []
 
 def insert_long_path(new_path):
-    if (len(long_path)<10):
+    if (len(long_path)<nb_long_path):
         long_path.append(new_path)
         return
     else:
-        for i in range(0,10):
+        for i in range(0,nb_long_path):
             if (len(new_path)>len(long_path[i])):
                 long_path[i] = new_path
                 return
             
-def display_too_long_path():
-    print("\n\n10 Longest path found")
-    for i in range(0,10):
+def display_too_long_path(logpath_file):
+    title = f"\n\n{nb_long_path} Longest path found"
+    print(title)
+    logpath_file.write(title+"\n")
+    for i in range(0,nb_long_path):
         print(long_path[i])
+        logpath_file.write(long_path[i]+"\n")
 
-def log_long_paths(directory, max_length=250):
-    with open("logPath.txt", "w", encoding="utf-8") as logpath_file, open("logCharset.txt", "w", encoding="utf-8") as logcharset_file:
+def log_long_paths(directory, max_length=255, max_file=128):
+    with open("log_check_path.txt", "w", encoding="utf-8") as logpath_file: #, open("logCharset.txt", "w", encoding="utf-8") as logcharset_file:
         for root, dirs, files in os.walk(directory):
             for file in files:
                 full_path = os.path.abspath(os.path.join(root, file))
@@ -30,16 +33,23 @@ def log_long_paths(directory, max_length=250):
                 # Log paths with length >= 250 characters
                 if len(full_path) >= max_length:
                     print(f"Long path (>= {max_length} chars): {full_path}")
-                    logpath_file.write(full_path + "\n")
+                    logpath_file.write(f"PATH{len(full_path)}--"+full_path + "\n")
+                    insert_long_path(full_path)
+
+                if len(file) > max_file:
+                    print(f"Long file (> {max_file} chars): {file}")
+                    logpath_file.write(f"FILE{len(file)}--"+full_path + "\n")
                     insert_long_path(full_path)
                 
                 # Check for problematic characters for file transfers (spaces, special characters, etc.)
                 # skipping 'C:'
-                if re.search(r'[^a-zA-Z0-9_\-\.\\/]', full_path[2:]):  
-                    print(f"Potentially problematic path: {full_path}")
-                    logcharset_file.write(full_path + "\n")
-        display_too_long_path()
+#                if re.search(r'[^a-zA-Z0-9_\-\.\\/]', full_path[2:]):  
+#                    print(f"Potentially problematic path: {full_path}")
+#                    logcharset_file.write(full_path + "\n")
+        display_too_long_path(logpath_file)
 
+
+    
 if __name__ == "__main__":
     directory_to_scan = input("Enter the path to the directory to scan : ")
     log_long_paths(directory_to_scan,250)
